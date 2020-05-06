1c85c2ac07cac1ffe8c34fa71b13b5673ef2800e
petermeissner <retep.meissner.com>
2020-05-06 22:19:46 +0200

    +  snap-2020-05-06

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
-# got to directory
-cd /home/peter/homepage
+docker run --rm \
+  --volume="$PWD:/srv/jekyll" \
+  -it jekyll/jekyll:stable \
+  jekyll build --trace
 
-# build page
-echo -e "$(tput setaf 2)\n. building\n$(tput setaf 7)"
-jekyll build
-
-# copy new page content into page directory
-echo -e "$(tput setaf 2)\n. copying\n$(tput setaf 7)"
-sudo rsync -r -u _site/* /var/www/html/petermeissner.de_root -r
+cp ./_site/* /home/peter/server/sites/petermeissner.de/ -r
